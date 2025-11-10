232b46dd00a6a605f67bc6e9d0391bc117da8689
KristRuzicka <krist.ruzickova@gmail.com>
2025-11-10 10:30:39 +0100

    main cycle updated, add function saves aslo task description remove fucntion - check for invalid input (str)

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ main.py: Task Manager
 author: Kristýna Růžičková
 email: krist.ruzickova@gmail.com
 """
-
-# List for saving tasks.
 tasks = []
 separator = 30*"="
 
@@ -14,18 +12,22 @@ def main_menu():
     "1. Add new task\n"
     "2. View all tasks\n" 
     "3. Remove task\n"
-    "4. End progam\n"
+    "4. End program\n"
     )
 
 # Takes input from user. Adds task to the list of tasks.
-def add_task(task_name, task_description):
+def add_task():
+    task_name = input("Enter task name:" )
+    task_description = input("Enter task description:" )
     while not task_name or not task_description:
         print("\nThis is an empty entry. Please enter valid value.")
         task_name = input("Enter task name:" )
         task_description = input("Enter task description:" )
     print(f"\nTask {task_name} has been added.")
     print(separator)
-    tasks.append(task_name)
+    tasks.append({"name": task_name,
+         "description": task_description})
+    return tasks
     
 # Prints a list of tasks with sequential number.
 def view_tasks():
@@ -34,7 +36,7 @@ def view_tasks():
         else:
             print("Added tasks:")
             for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
+                print(f"{i}. {task["name"]} - {task["description"]}")
         print(separator)
 
 # Removes selected task from the list of tasks.
@@ -42,12 +44,18 @@ def remove_task():
     if len(tasks) == 0:
         print("\nNo tasks have been added yet.")
     else:
-        task_number = int(input("Enter number of task which should be removed:" ))
-        if task_number not in range(1, len(tasks)+1):
-            print(f"\nThis task number is not listed.")
-        else: 
-            tasks.pop(task_number-1)
-            print(f"\nRemoving task number {task_number}.")
+        view_tasks()
+        try:
+            task_number = int(input("Enter number of task which should be removed: "))
+
+            if task_number not in range(1, len(tasks)+1):
+                print(f"\nThis task number is not listed.")
+            else: 
+                print(f"Task number {task_number} has been removed.")
+                tasks.pop(task_number-1)
+        except ValueError:
+            print("Please enter a valid number.")
+
     print(separator)
 
 # Main loop
@@ -63,17 +71,11 @@ while True:
     choice = int(choice)
     
     if choice == 1:
-        task_name = input("Enter task name:" )
-        task_description = input("Enter task description:" )
-        add_task(task_name, task_description)
+        add_task()
     elif choice == 2:
         view_tasks()
     elif choice == 3:
-        if len(tasks) == 0:
-            print("No task to remove.")
-        else:
-            view_tasks()
-            remove_task()
+        remove_task()
     elif choice == 4:
         print("\nEnding program.")
         break
